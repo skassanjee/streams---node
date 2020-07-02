2cb1453ebeacce49d386acf7de104505579eae35
Sahil <skassanjee@gmail.com>
2020-07-02 10:16:58 +0000

    serving json

--- index.js
+++ index.js
@@ -34,3 +34,48 @@ myWriteStreamm.write(chunk)
 
 
 //pipes
+//pipes take read streams and 'pipes' it to a write stream
+//automatically to write stream
+
+myReadStream.pipe(myWriteStream);
+//does the same thing as the above method
+
+
+//we can put this code
+
+my.ReadStream.pip(res)
+
+
+//into the server and we can stream the data to the user
+//summary
+//create server and header
+//create read stream
+//pipe the data to the user  through the res
+
+
+
+
+
+
+
+
+//serving html pages to the client/browser/user
+
+//first we create an HTML page (like normal)
+//change the header to text/html
+//change the file in fs.createReadStream to the html file
+//html will be rendered
+
+
+
+
+
+
+
+//serving JSON
+///change header to application/JSON
+res.end(JSON.stringify(myObj));
+//we use this to turn obj into json
+//json is returned
+//why json:
+//we can return JSON, then javascript can do something
